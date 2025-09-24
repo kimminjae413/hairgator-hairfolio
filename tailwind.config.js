ee062aa1993ff4a7ae0fc3b9eab4c8a302493881
kimminjae413 <eric708@naver.com>
2025-09-24 17:57:38 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'sm:inline',
+    'sm:flex-row',
+    'sm:items-center',
+    'flex-wrap',
+    'gap-4',
+    'whitespace-nowrap',
+    'text-ellipsis',
+    'overflow-hidden',
+    'sm:w-auto',
+    'w-full'
+  ],
   theme: {
     extend: {
       fontFamily: {
